b05be091d27af3d897c8dba915ef6b95e9511ab1
Nathan Mundo <nathanmundo@icloud.com>
2024-07-09 11:45:23 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,97 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
+import {
+  Container,
+  Grid,
+  TextField,
+  Typography,
+  Button,
+  Card,
+} from "@mui/material";
+import TodosList from "./TodosList";
 
 function App() {
+  const [todos, setTodos] = useState(
+    JSON.parse(localStorage.getItem("todos")) || []
+  );
+  const [todo, setTodo] = useState("");
+
+  useEffect(() => {
+    localStorage.setItem("todos", JSON.stringify(todos));
+  }, [todos]);
+
+  const addTodo = () => {
+    const newTodo = {
+      id: crypto.randomUUID(),
+      text: todo,
+      completed: false,
+    };
+    setTodos([...todos, newTodo]);
+    setTodo("");
+  };
+
+  const toggleTodo = (id) => {
+    const newTodos = todos.map((todo) =>
+      todo.id === id ? { ...todo, completed: !todo.completed } : todo
+    );
+    setTodos(newTodos);
+  };
+
+  const deleteTodo = (id) => {
+    const newTodos = todos.filter((todo) => todo.id !== id);
+    setTodos(newTodos);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container maxWidth="sm" sx={{ paddingBottom: 5 }}>
+      <Grid container spacing={2}>
+        <Grid item xs={12}>
+          <Typography variant="h3">To Do List</Typography>
+        </Grid>
+        <Grid item xs={12} md={10}>
+          <TextField
+            value={todo}
+            onChange={(event) => setTodo(event.target.value)}
+            onKeyDown={(event) => event.key === "Enter" && addTodo()}
+            size="small"
+            placeholder="Type in something..."
+            fullWidth
+          />
+        </Grid>
+        <Grid item xs={12} md={2}>
+          <Button
+            variant="contained"
+            size="medium"
+            onClick={() => addTodo()}
+            fullWidth
+          >
+            Add
+          </Button>
+        </Grid>
+        <Grid item xs={12}>
+          <Card>
+            <TodosList
+              todos={todos}
+              toggleTodo={toggleTodo}
+              deleteTodo={deleteTodo}
+            />
+          </Card>
+        </Grid>
+        <Grid item xs={12}>
+          <Typography variant="h6">Completed</Typography>
+        </Grid>
+        <Grid item xs={12}>
+          <Card>
+            <TodosList
+              todos={todos}
+              toggleTodo={toggleTodo}
+              deleteTodo={deleteTodo}
+              completedType
+            />
+          </Card>
+        </Grid>
+      </Grid>
+    </Container>
   );
 }
 
---
+++ src/TodosList.jsx
@@ -0,0 +1,57 @@
+import { Fragment } from "react";
+import {
+  ListItem,
+  IconButton,
+  ListItemButton,
+  ListItemIcon,
+  Checkbox,
+  List,
+  ListItemText,
+  Divider,
+} from "@mui/material";
+import DeleteIcon from "@mui/icons-material/Delete";
+
+const TodosList = ({
+  todos,
+  deleteTodo,
+  toggleTodo,
+  completedType = false,
+}) => (
+  <List>
+    {todos.filter(({ completed }) => completed).length === 0 && (
+      <ListItem>Get something done!</ListItem>
+    )}
+    {todos
+      .filter(({ completed }) => completedType === completed)
+      .map(({ id, text, completed }, index) => (
+        <Fragment key={id}>
+          {index !== 0 && <Divider />}
+          <ListItem
+            secondaryAction={
+              <IconButton
+                onClick={() => deleteTodo(id)}
+                edge="end"
+                aria-label="delete"
+              >
+                <DeleteIcon />
+              </IconButton>
+            }
+          >
+            <ListItemButton onClick={() => toggleTodo(id)}>
+              <ListItemIcon>
+                <Checkbox
+                  edge="start"
+                  checked={completed}
+                  tabIndex={-1}
+                  disableRipple
+                />
+              </ListItemIcon>
+              <ListItemText id={id} primary={text} />
+            </ListItemButton>
+          </ListItem>
+        </Fragment>
+      ))}
+  </List>
+);
+
+export default TodosList;
